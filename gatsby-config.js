91cd3a55d313036e0a8f8970daa71b4e93a1d865
panosmakris <53366987+panosmakris@users.noreply.github.com>
2019-09-16 14:35:26 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         contentPosts: "content/posts",
         contentAuthors: "content/authors",
         basePath: "/",
-        authorsPage: false,
+        authorsPage: true,
         sources: {
           local: true,
           contentful: true,
